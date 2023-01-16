a1e3fdd272595368068efe77ffbf47802e766840
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-16 04:26:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ import os
 # Autenticación de OpenAI (oculta la clave en una variable de entorno)
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-
 def generate_article(title, outline):
-  model_engine = "text-davinci-003"
-  prompt = (f"Escribe un artículo titulado '{title}' basado en el siguiente esquema:\n{outline}")
+    model_engine = "text-davinci-003"
+    prompt = (f"Escribe un artículo titulado '{title}' basado en el siguiente esquema:\n{outline}")
 
-  completions = openai.Completion.create(
-      engine=model_engine,
-      prompt=prompt,
-      max_tokens=3824,
-      n=1,
-      stop=None,
-      temperature=0.8,
-  )
+    completions = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=3824,
+        n=1,
+        stop=None,
+        temperature=0.8,
+    )
 
-  article = completions.choices[0].text
-  return article
+    article = completions.choices[0].text
+    return article
 
 st.title("Generador de artículos")
 
 sidebar = st.sidebar
 title = sidebar.text_input("Título:")
-outline = sidebar.text_area("Esquema:")
+outline = sidebar.text_area("Esquema o partes:")
 
 if st.button("Generar artículo"):
-  article = generate_article(title, outline)
-  st.success(article)
+    article = generate_article(title, outline)
+    st.success(article)
+
 if st.button("Exportar artículo"):
     from docx import Document
     document = Document()
